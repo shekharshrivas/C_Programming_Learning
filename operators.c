ee5fbca3a4e3f4a3a338c2f4f85603ee00e47509
Shekhar <shekharshrivas07@gmail.com>
2024-07-09 23:38:28 +0530

    operators in c

---
+++ operators.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+   int a, b;
+   a = 34;
+   b = 6;
+   printf("a + b = %d\n", a+b);
+   printf("a - b = %d\n", a-b);
+   printf("a * b = %d\n", a*b);
+   printf("a / b = %d\n", a/b);
+   printf("a % b = %d\n", a%b);
+
+   float c = 6.2;
+
+   printf("Values for float number\n");
+   printf("a + b = %f\n", a+c);
+   printf("a - b = %f\n", a-c);
+   printf("a * b = %f\n", a*c);
+   printf("a / b = %f\n", a/c);
+//    printf("a % b = %d\n", a%b);
+
+   return 0;
+}
